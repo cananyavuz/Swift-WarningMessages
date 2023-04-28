176e9c88800e61f2035fc0416f55e83e6756b43c
cananyavuz <yavuzcanan15@gmail.com>
2023-04-28 11:13:11 +0300

    Warning Messages added.

--- WarningMessages/ViewController.swift
+++ WarningMessages/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var password2TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func createdWarning(titleInput : String , messageInput : String ){
+        let warningMessage = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default){
+            (UIAlertAction) in
+            //when OK Button run
+            print("OK Button run")
+        }
+        
+        warningMessage.addAction(okButton)
+        
+        self.present(warningMessage, animated: true)
+    }
+    
+    @IBAction func SignUpClick(_ sender: Any) {
+        
+        if emailTextField.text == "" {
+            
+            createdWarning(titleInput: "Error", messageInput: "Wrong E-mail!")
+            
+        }else if passwordTextField.text == ""{
+            
+            createdWarning(titleInput: "Error", messageInput: "Wrong Password!")
+            
+        }else if password2TextField.text == ""{
+           
+            createdWarning(titleInput: "Error", messageInput: "Wrong Password!")
+            
+        }else if passwordTextField.text != password2TextField.text{
+            
+            createdWarning(titleInput: "Error", messageInput: "Passwords are not same!")
+    
+        }else{
+            //successfull
+            createdWarning(titleInput: "Congratulations!", messageInput: "User Created")
+        }
+        
+        
+    }
+    
 }
 
